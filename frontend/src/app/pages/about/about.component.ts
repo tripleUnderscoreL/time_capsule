02378d1a4b94089155cabe3d5f04b94cb12d295d
owbfk <artem.volchihin@mail.ru>
2024-05-22 15:09:51 +0500

    Add page about and footer fix

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
