85f86998f709f8e745c60e789826d2c65c6c9894
owbfk <artem.volchihin@mail.ru>
2024-06-15 21:17:20 +0500

    small frontend fix 2.0

--- frontend/src/app/pages/reviews/reviews.component.ts
+++ frontend/src/app/pages/reviews/reviews.component.ts
@@ -43,7 +43,7 @@ export class ReviewsComponent {
         error: error => console.log(error),
         }
       );
-      //window.location.reload();
+      window.location.reload();
     }
     else {
       console.log("Не все поля заполнены")
